e8681defb00e635ec4f5251a51f9e38fa7471612
Stanislav Hriaznov <st.gryaznov@gmail.com>
2021-09-02 14:09:02 +0300

    Fixnum -> Integer

--- spec/dummy/app/assets/javascripts/prism_rails.js
+++ spec/dummy/app/assets/javascripts/prism_rails.js
@@ -7,7 +7,7 @@ Prism.languages.ruby = {
     lookbehind: true
   },
   'keyword': /\b(alias|and|BEGIN|begin|break|case|class|def|define_method|defined|do|each|else|elsif|END|end|ensure|false|for|if|in|module|new|next|nil|not|or|raise|redo|rescue|retry|return|self|super|then|throw|true|undef|unless|until|when|while|yield)\b/g,
-  'builtin': /\b(Array|Bignum|Binding|Class|Continuation|Dir|Exception|FalseClass|File|Stat|File|Fixnum|Fload|Hash|Integer|IO|MatchData|Method|Module|NilClass|Numeric|Object|Proc|Range|Regexp|String|Struct|TMS|Symbol|ThreadGroup|Thread|Time|TrueClass)\b/,
+  'builtin': /\b(Array|Bignum|Binding|Class|Continuation|Dir|Exception|FalseClass|File|Stat|File|Integer|Fload|Hash|Integer|IO|MatchData|Method|Module|NilClass|Numeric|Object|Proc|Range|Regexp|String|Struct|TMS|Symbol|ThreadGroup|Thread|Time|TrueClass)\b/,
   'boolean': /\b(true|false)\b/g,
   'number': /\b-?(0x)?\d*\.?\d+\b/g,
   'operator': /[-+]{1,2}|!|=?&lt;|=?&gt;|={1,2}|(&amp;){1,2}|\|?\||\?|\*|\//g,
